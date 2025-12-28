382f385df3e7681f0fd9900e2285b52e98fb5231
heajun <heajun35@gmail.com>
2025-12-29 01:33:21 +0900

    Feat: introduce 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Introduce from '@/component/introduce/Introduce'
 import { SectionID } from '@/constant/sectionId'
 
 export default function Home() {
   return (
     <main id={SectionID.mainContent} style={{ filter: 'none' }}>
-      test
+      <div className="mx-auto max-w-7xl px-20 max-sm:px-10">
+        <div className="mx-auto max-w-[1000px]">
+          <Introduce />
+        </div>
+      </div>
     </main>
   )
 }
--- src/component/introduce/Introduce.tsx
+++ src/component/introduce/Introduce.tsx
@@ -3,48 +3,49 @@
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 import { FadeUp } from '@/constant/framer-motion'
-import { Links } from '@/constant/link'
-import { useAppSelector } from '@/hooks/useReactRedux'
 import { BlogIcon, GitHubIcon } from '../common/header/react-icons'
+import { Links } from '@/constant/link'
+import { useLoadingStore } from '@/store/useLoadingStore'
 
 const ICONLIST = [
-  { link: Links.tstory, ICON: <BlogIcon size={30} /> },
+  { link: Links.velog, ICON: <BlogIcon size={30} /> },
   { link: Links.github, ICON: <GitHubIcon size={30} /> },
 ]
 
 export default function Introduce() {
-  const isFinishMainLoading = useAppSelector(
-    (s) => s.mainLoading.isFinshLoading
-  )
+  const { isFinishing } = useLoadingStore()
+
   return (
     <motion.section
       className="flex h-screen flex-col justify-center"
       initial="init"
-      animate={isFinishMainLoading ? 'on' : 'init'}
+      animate={isFinishing ? 'on' : 'init'}
       transition={{ staggerChildren: 0.2, delayChildren: 1 }}
     >
-      <motion.h1 className="text-green pb-8 text-2xl" variants={FadeUp}>
-        안녕하세요. 저는 웹 개발자
+      <motion.h1 className="text-accent-100 pb-8 text-2xl" variants={FadeUp}>
+        프론트엔드 개발자
       </motion.h1>
       <motion.h2
-        className="text-lightest-slate pb-6 text-6xl font-bold"
+        className="text-accent-200 pb-6 text-6xl font-bold"
         variants={FadeUp}
       >
-        유승윤입니다.
+        정해준 입니다.
       </motion.h2>
-      <motion.h2 className="pb-8 text-6xl font-bold" variants={FadeUp}>
-        저는 끊임없는 도전을 즐기며,
-        <br /> 웹 개발의 매력에 빠져 있습니다.
+      <motion.h2
+        className="text-primary-300 pb-8 text-4xl font-bold"
+        variants={FadeUp}
+      >
+        문제를 끝까지 파고들며,
+        <br /> 작은 개선을 반복하며 더 나은 웹 경험을 만들어갑니다.
       </motion.h2>
       <motion.p
-        className="max-w-[580px] pb-8 text-xl break-keep"
+        className="text-text-200 max-w-[580px] pb-8 text-xl break-keep"
         variants={FadeUp}
       >
-        사용자에게 먼저 다가갈 수 있는 웹사이트의 프론트엔드 부분을 담당하고
-        있습니다. 제 웹 포트폴리오를 방문해 주셔서 진심으로 감사드립니다.
+        제 웹 포트폴리오를 방문해 주셔서 진심으로 감사드립니다.
       </motion.p>
 
-      <motion.ol className="text-green flex gap-4" variants={FadeUp}>
+      <motion.ol className="text-primary-200 flex gap-4" variants={FadeUp}>
         {ICONLIST.map((obj, i) => (
           <Link key={i} href={obj.link} target="_blank">
             <li>{obj.ICON}</li>
